e63b2486b0aacdc17fe1e2f17d0bea92dd6c8678
Ibrahim Selmanov <ibrahim.sv@pm.me>
2025-02-28 20:54:31 +0100

    Changes

--- src/index.vue
+++ src/index.vue
@@ -8,17 +8,11 @@ export default {
 </script>
 
 <template>
-    <div class="flex flex-col items-center h-screen">
+    <div class="flex flex-col items-center h-screen bg-offWhite">
         <Header></Header>
         <!-- Head -->
-        <div class="w-full h-80 flex flex-row justify-start">
-            <div class="ml-32 mt-12 w-7/12">
-                <h1 class="text-7xl leading-tight font-bold">Je m'appelle Ibrahim,<br>un étudiant en informatique<span class="text-gray-500"><br>basé en Belgique.</span></h1>
-                <div class="text-justify font-medium">
-                    <p class="">
-                    </p>
-                </div>
-            </div>
+        <div class="w-full h-full flex flex-row justify-start">
+            
         </div>
     </div>
 </template>
