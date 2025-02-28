f629cff89b1f9821c6b09dbadcfefa578f309d44
Ibrahim Selmanov <ibrahim.sv@pm.me>
2025-02-28 17:02:02 +0100

    Big changes

---
+++ src/components/header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full h-auto flex flex-row justify-evenly   pt-8 pb-8">
+        <div>
+            <ul class="text-3xl list-none text-right    ">
+                <li class="hover:transition delay-50 duration-200 ease-in-out hover:-translate-x-2 hover:scale-110"><a href="#">ACCUEIL</a></li>
+                <li class="pr-8 hover:transition delay-50 duration-200 ease-in-out hover:-translate-x-2 hover:scale-110"><a href="#">À PROPOS DE MOI</a></li>
+                <li class="pr-16 hover:transition delay-50 duration-200 ease-in-out hover:-translate-x-2 hover:scale-110"><a href="#">BLOG</a></li>
+            </ul>
+        </div>
+        <div class="">
+            <p class="text-center text-xl">Envoyez-moi un<br></p>
+        </div>
+    </div>
+</template>
--- src/index.vue
+++ src/index.vue
@@ -1,23 +1,23 @@
 <script>
+import Header from './components/header.vue';
 export default {
-
+    components: {
+        Header
+    }
 }
 </script>
 
 <template>
     <div class="flex flex-col items-center h-screen">
-        <div class="w-full h-auto flex flex-row justify-evenly pt-8 pb-8">
-            <ul class="text-xl list-none text-left">
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">ABOUT</a></li>
-                <li><a href="#">CONTACT</a></li>
-            </ul>
-            <h1 class="text-4xl font-medium">IBRAHIM SELMANOV</h1>
-        </div>
+        <Header></Header>
         <!-- Head -->
-        <div class="w-full h-80 flex flex-row">
-            <div class="">
-                
+        <div class="w-full h-80 flex flex-row justify-start">
+            <div class="ml-32 mt-12 w-7/12">
+                <h1 class="text-7xl leading-tight font-bold">Je m'appelle Ibrahim,<br>un étudiant en informatique<span class="text-gray-500"><br>basé en Belgique.</span></h1>
+                <div class="text-justify font-medium">
+                    <p class="">
+                    </p>
+                </div>
             </div>
         </div>
     </div>
